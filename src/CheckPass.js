edbaa4fd97306153e1ae84e631b4343c92929a2f
janeth <jarri023@fiu.edu>
2020-05-04 00:37:57 -0400

    background/ui update

--- src/CheckPass.js
+++ src/CheckPass.js
@@ -1,6 +1,39 @@
 import React, { Component } from "react";
 
 class CheckPass extends Component {
+  constructor() {
+    super();
+    this.state = {
+      password: "",
+      isHacked: "",
+    };
+  }
+
+  handleChange = (e) => {
+    this.setState({ [e.target.id]: e.target.value });
+  };
+
+  verifyPassword = e => {
+    e.preventDefault();
+    console.log("entered post request");
+    const { password } = this.state;
+    let result = fetch(
+      "https://password-checker-api.herokuapp.com/check-pass",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ password }),
+      }
+    );
+    let response = result.then((res) => {
+      res.json();
+      console.log(res);
+    });
+    response.then((data) =>
+      this.setState({ isHacked: data }, () => console.log(this.state.isHacked))
+    );
+  };
+
   render() {
     return (
       <div className="check-pass-box">
@@ -14,10 +47,18 @@ class CheckPass extends Component {
           <br />
           <br />
         </div>
-        <form>
-          <input type="text" className="check-input" />
-          <button className="check-btn">Check</button>
+        <form type="submit">
+          <input
+            type="text"
+            className="check-input"
+            id="password"
+            onChange={this.handleChange}
+          />
+          <button className="check-btn" onClick={this.verifyPassword}>
+            Check
+          </button>
         </form>
+        {this.state.isHacked}
       </div>
     );
   }
